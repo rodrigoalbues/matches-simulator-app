beec54077ad1ca85a0bfe087b9d4c1049c56dd3a
rodrigoalbues <95535393+rodrigoalbues@users.noreply.github.com>
2022-06-23 00:39:04 -0400

    Created model/entity

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package me.dio.simulator;
+package me.dio.simulator.ui;
 
 import android.os.Bundle;
 
@@ -17,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setupMatchesList();
+        setupMatchesRefresh();
+        setupFloatingActionButton();
+    }
+
+    private void setupMatchesList() {
+        //TODO Listar as partidas, consumindo nossa API.
+    }
+
+    private void setupMatchesRefresh() {
+        //TODO Atualizar as partidas na ação swipe.
+    }
+
+    private void setupFloatingActionButton() {
+        //TODO Criar evento de click e simulação de partidas.
     }
 }
